ec44c1e626e8c3602dcd382fafbd4d67df65af1e
inesseq <inesseq@gmail.com>
2018-02-13 11:10:48 +0000

    fixed .env.template

--- angular-src/.env.template.js
+++ angular-src/.env.template.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // This file is a template. Rename the file to ".env".
 // Replace the corresponding variables.
 // Don't commit it, since it is sensitive info!
-exports.secrets = {
+exports.Vars = {
     FENIX_CLIENT_ID: "GCE",
     REDIRECT_URL: "ROCKS",
     REDIRECT_URL_PROF: "YEAH"
--- angular-src/.env.template.ts
+++ angular-src/.env.template.ts
@@ -1,7 +1,7 @@
 // This file is a template. Rename the file to ".env".
 // Replace the corresponding variables.
 // Don't commit it, since it is sensitive info!
-export const secrets = {
+export const Vars = {
     FENIX_CLIENT_ID: "GCE",
     FENIX_CLIENT_SECRET : "ROCKS",
     REDIRECT_URL: "OH",
