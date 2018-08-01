a5bd66a0e526a1c26c7b6166c197f98215a04020
DuTheo <ldptek@gmail.com>
2018-08-01 11:58:00 +0100

    - Changed app component to hide navbar and footer on homepage (adding a new control variable ('controlVariable') for that purpose; - New homepage focused on minimalism and functionality.

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'GCE';
+  controlVariable: boolean;
+
+  onActivate(component) {
+    this.controlVariable = !component.isHome;
+  }
 }
