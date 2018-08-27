77c51ffbac7ab6a6149d287cd0fcdec3c95ca79a
rafaelapb <rafaelp.belchior@gmail.com>
2018-08-27 14:57:02 +0100

    Updated GCE_BASE

--- gce_base.js
+++ gce_base.js
@@ -1,12 +1,27 @@
 /*
 * GCE-THESIS
-* This project has been made by the GCE's team:
+* This project has been started by the GCE's team:
 * Rafael Belchior
 * Daniel Ramos
 * Inês Sequeira
 *
 * Thanks to António Higgs for the early support
 * */
+
+// Modules =================================================
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const logger = require('./server/log/logger');
+const fs = require('fs');
+const  helmet = require('helmet');
+const Utils = require('./server/mongodb/accesses/utils-accesses');
+const schedule = require('node-schedule');
+const ba_logger = require('./server/log/ba_logger');
+const  spawn = require('child_process').spawn;
 
 //MAIN Env. variables, needed to the project to run
 const node_env = process.env.NODE_ENV;
@@ -23,42 +38,44 @@ const FENIX_CLIENT_SECRET = process.env.FENIX_CLIENT_SECRET;
 const REDIRECT_URL_PROF = process.env.REDIRECT_URL_PROF;
 const REDIRECT_URL = process.env.REDIRECT_URL;
 
-console.log("NODE_ENV = " + node_env);
-console.log("DB_BACKUP = " + DB_BACKUP);
-
 const NODE_ENV_ERROR =
-    "[ERROR] - NODE_ENV not set.\n" +
-    "Define it on your environment variables.";
+  "[ERROR] - NODE_ENV not set.\n" +
+  "Define it on your environment variables.";
 
 if (!node_env) {
     return console.error(NODE_ENV_ERROR);
 }
 
-if (!DB_SECRET || !FENIX_CLIENT_ID ||
-        !FENIX_CLIENT_SECRET || !REDIRECT_URL_PROF ||
-        !REDIRECT_URL) {
-    console.log("There are one or more environment variables " +
-        "not set.");
+logger.info("NODE_ENV = " + node_env);
+if (DB_SECRET)  {
+    logger.info("DB_SECRET = " + "DEFINED");
+}   else    {
+    logger.warn("DB_SECRET = " + "NOT DEFINED");
 }
+
+logger.info("DB_PRODUCTION = " + DB_PRODUCTION);
+logger.info("DB_BACKUP = " + DB_BACKUP);
+logger.info("DB_BACKUP_PRODUCTION_URI = " + DB_BACKUP_PRODUCTION_URI);
+logger.log("DB_USER = " + DB_USER);
+
+if (DB_PASSWORD)  {
+    logger.info("DB_PASSWORD = " + "DEFINED");
+}   else    {
+    logger.warn("DB_PASSWORD = " + "NOT DEFINED");
+}
+
+logger.info("FENIX_CLIENT_ID = " + FENIX_CLIENT_ID);
+if (FENIX_CLIENT_SECRET)  {
+    logger.info("FENIX_CLIENT_SECRET = " + "DEFINED");
+}   else    {
+    logger.warn("FENIX_CLIENT_SECRET = " + "NOT DEFINED");
+}
+logger.info("REDIRECT_URL = " + REDIRECT_URL);
 
 if(!DB_PRODUCTION && node_env === "production") {
     console.error("Production DB not set. Please export the environment variable");
 }
 
-// Modules =================================================
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const logger = require('./server/log/logger');
-const fs = require('fs');
-const  helmet = require('helmet');
-const Utils = require('./server/mongodb/accesses/utils-accesses');
-const schedule = require('node-schedule');
-const ba_logger = require('./server/log/ba_logger');
-const  spawn = require('child_process').spawn;
 // Configuration ===========================================
 let dbConfig = require('./config/db');
 let appConfig = require('./config/app');
@@ -67,7 +84,7 @@ let port = process.env.PORT || appConfig.ports[node_env];
 // Connect to mongoDB database ===========================================
 mongoose.connect(dbConfig.urls[node_env],{useMongoClient: true});
 mongoose.connection.on("connected", () => {
-    console.log("Connected to MongoDB on " + dbConfig.urls[node_env]);
+    logger.info("Connected to MongoDB on " + dbConfig.urls[node_env]);
 });
 mongoose.connection.on("error", (dbError) => {
     logger.error("Could not connect to database on: " + dbConfig.urls[node_env]);
@@ -76,12 +93,10 @@ mongoose.connection.on("error", (dbError) => {
 
 if(DB_BACKUP)   {
     //Run at ten of Thursdays
-    const backup = schedule.scheduleJob('0 22 * * 4', () =>   {
+    schedule.scheduleJob('0 22 * * 4', () =>   {
     ba_logger.ba("BA|"+ "DB_BACKUP|" + new Date().toJSON().slice(0,16).replace(/-/g,'/')+ "h");
-
-    console.log("====================");
-    console.log(new Date().toJSON().slice(0,19).replace(/-/g,'/')+ "h");
-    console.log("Backup is on");
+    logger.info(new Date().toJSON().slice(0,19).replace(/-/g,'/')+ "h");
+    logger.info("Backup is on");
 
     const backupPathUsers = "backup/backup-Users-" + Date.now() + ".json";
     const backupUsers =   spawn('mongoexport', [
@@ -90,15 +105,15 @@ if(DB_BACKUP)   {
         backupPathUsers
     ]);
 
-    backupUsers.stderr.on('data', function(data) {
-        console.log('Users: stderr: ' + data);
+    backupUsers.stderr.on('data', (data) => {
+        logger.info('Users: stderr: ' + data);
     });
 
-    backupUsers.on('exit', function(code) {
+    backupUsers.on('exit', (code) => {
         if(code === 0)   {
-            console.log("Users: Successfully backed up");
+            logger.info("Users: Successfully backed up");
         }   else {
-            console.log("Users: Error with code: " + code);
+            logger.info("Users: Error with code: " + code);
         }
     });
 
@@ -109,20 +124,20 @@ if(DB_BACKUP)   {
         backupPathSignup
     ]);
 
-        backupSignups.stdout.on('data', function(data) {
-            console.log('Signups: stdout: ' + data);
+        backupSignups.stdout.on('data', (data) => {
+            logger.info('Signups: stdout: ' + data);
         });
 
-        backupSignups.stderr.on('data', function(data) {
-            console.log('Signups: stderr: ' + data);
+        backupSignups.stderr.on('data', (data) => {
+            logger.info('Signups: stderr: ' + data);
             //Here is where the error output goes
         });
 
-        backupSignups.on('exit', function(code) {
+        backupSignups.on('exit', (code) => {
             if(code === 0)   {
-                console.log("Signups: Successfully backed up");
+                logger.info("Signups: Successfully backed up");
             }   else {
-                console.log("Signups: Error with code: " + code);
+                logger.info("Signups: Error with code: " + code);
             }
         });
 
@@ -133,21 +148,19 @@ if(DB_BACKUP)   {
         backupPathFeedback
     ]);
 
-        backupFeedback.stderr.on('data', function(data) {
-            console.log('Feedbacks: stderr: ' + data);
+        backupFeedback.stderr.on('data', (data) => {
+            logger.info('Feedbacks: stderr: ' + data);
             //Here is where the error output goes
         });
 
-        backupFeedback.on('exit', function(code) {
+        backupFeedback.on('exit', (code) => {
             if(code === 0)   {
-                console.log("Feedbacks: Successfully backed up");
+                logger.info("Feedbacks: Successfully backed up");
             }   else {
-                console.log("Feedbacks: Error with code: " + code);
+                logger.info("Feedbacks: Error with code: " + code);
             }
         });
 
-
-        console.log("====================");
     });
 }
 
@@ -159,9 +172,9 @@ app.use(morgan('dev'));
 
 //Full log to file
 app.use((morgan)("common", {stream: logger.stream}));
+
 // Security ===========================================
 //Defaults:
-
 helmet.contentSecurityPolicy({
     directives: {
         defaultSrc: ["'self'"],
@@ -171,9 +184,6 @@ helmet.contentSecurityPolicy({
 
 app.use(helmet({
 }));
-
-//Logger
-console.log("Winston is Overriding 'Express' logger");
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -202,22 +212,9 @@ process.on('uncaughtException', (err) => {
     }
     process.exit();
 });
-/*
-//For manual installation of a SSL certificate.
-const privateKey  = [fs.readFileSync('./sslcert/gce.key', 'utf8')];
-const certificate = fs.readFileSync('./sslcert/gce-neiist_org.crt', 'utf8');
-
-// your express configuration here
-let credentials = {key: privateKey, cert: certificate};
-let httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, () => {//https is 8443
-    logger.warn("Server running on port: " + port);
-    logger.warn("Server running since: " + Utils.time);
-});
-*/
 
 //We use Heroku, so we delegate SSL implementation to Heroku's load balancer.
+
 app.listen(port, () => {
         logger.info("Server running on port: " + port);
         logger.warn("Server running since: " + Utils.time);
