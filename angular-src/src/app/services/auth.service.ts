13d3e85f9a7c44a794622ada748bd105fefc1950
rafaelapb <rafaelp.belchior@gmail.com>
2018-09-11 23:56:00 +0100

    other: updates dependencies and migrates to Angular 6

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers} from "@angular/http";
 import { environment } from './../../environments/environment';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
+
 import { tokenNotExpired } from 'angular2-jwt';
 @Injectable()
 export class AuthService {
@@ -14,7 +15,7 @@ export class AuthService {
   authUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('users/login', user, {headers: headers}).map(res => res.json());
+    return this.http.post('users/login', user, {headers: headers}).pipe(map(res => res.json()));
   }
 
   //Checks if user is logged in
@@ -72,20 +73,20 @@ export class AuthService {
   registerStudent(token) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('student/register', { 'tokenq': token }, {headers: headers}).map(res => res.json());
+    return this.http.post('student/register', { 'tokenq': token }, {headers: headers}).pipe(map(res => res.json()));
   }
 
   //Mock up. Should send token
   registerProfessor(token) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('professor/register', { 'tokenq': token }, {headers: headers}).map(res => res.json());
+    return this.http.post('professor/register', { 'tokenq': token }, {headers: headers}).pipe(map(res => res.json()));
  }
 
   registerCompany(company) {
     let headers = new Headers();
     headers.append('Content-Type','application/json');
-    return this.http.post('company/register', company, {headers: headers}).map(res => res.json());
+    return this.http.post('company/register', company, {headers: headers}).pipe(map(res => res.json()));
  }
 
 }
