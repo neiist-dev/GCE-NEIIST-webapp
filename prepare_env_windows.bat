a6a3423f68a793b93e117b35142b18c940e2ad7c
jptrindade <joao.pedro.trindade@tecnico.ulisboa.pt>
2019-02-07 12:41:30 +0000

    fix: fixes security problems

--- prepare_env_windows.bat
+++ prepare_env_windows.bat
@@ -2,7 +2,7 @@
 :: setx.exe does not set the environment variable in the current command prompt, but it will be available in subsequent command prompts.
 
 setx NODE_ENV "development"
-setx DB_PRODUCTION "mongodb://jptrindade:d6C3KCPqbuTcRyQ@ds263571.mlab.com:63571/gce_base"
+setx DB_PRODUCTION "mongodb://<dbuser>:<dbpassword>@<dbid>.mlab.com:63571/gce_base"
 setx DB_SECRET "secret"
 setx FENIX_CLIENT_ID "1977390058176570"
 setx FENIX_CLIENT_SECRET "5fMaQkZjor0x+0VmBSntVx54CbuZf/PYenLziqp3kVYdLmHzzn/cNJkY26avmXVrGjlYZHPOlewBEip4at6plA=="
