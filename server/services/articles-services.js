5451bfca61efb368c24c0875248304e9644b0c7c
danielsantosfernandes <dani.s-fernandes@hotmail.com>
2019-11-26 11:41:44 +0000

    fix: display article date

--- server/services/articles-services.js
+++ server/services/articles-services.js
@@ -6,6 +6,7 @@ class ArticlesServices {
     constructor() {
         this.getArticles = getArticles;
         this.classifyArticleByTags = classifyArticleByTags;
+        this.parseDate = parseDate;
     }
 }
 
@@ -19,25 +20,32 @@ async function getArticles() {
                 reject(err);
             }
 
-            let articles = [];
-            for (let i = 0; i < rss.length; i++) {
-                let new_article = {};
-                
-                new_article.title = rss[i].title;
-                dom = new JSDOM("<!DOCTYPE html>" + rss[i].description);
-                new_article.description = dom.window.document.querySelector('p').textContent;
-                new_article.date = rss[i].date;
-                new_article.link = rss[i].link;
-                new_article.author = rss[i].author;
-                new_article.comments = rss[i].comments;
-                new_article.categories = rss[i].categories;
-                new_article.image = dom.window.document.querySelector('img').src;
-                new_article.tags = classifyArticleByTags(new_article);
+            try {
+                let articles = [];
+                for (let i = 0; i < rss.length; i++) {
+                    let new_article = {};
+                    
+                    new_article.title = rss[i].title;
+                    dom = new JSDOM("<!DOCTYPE html>" + rss[i].description);
+                    new_article.description = dom.window.document.querySelector('p').textContent;
+                    new_article.date = parseDate(rss[i].date);
+                    new_article.link = rss[i].link;
+                    new_article.author = rss[i].author;
+                    new_article.comments = rss[i].comments;
+                    new_article.categories = rss[i].categories;
+                    new_article.image = dom.window.document.querySelector('img').src;
+                    new_article.tags = classifyArticleByTags(new_article);
 
-                articles.push(new_article);
+                    console.log(rss[i].categories)
+
+                    articles.push(new_article);
+                }
+
+                resolve(articles);
             }
-
-            resolve(articles);
+            catch(e) {
+                reject(e)
+            }
         });
     });
 
@@ -50,4 +58,9 @@ function classifyArticleByTags(article) {
         tags.push("Article");
     }
     return tags;
+}
+
+function parseDate(d) {
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    return months[d.getMonth()] + " " + d.getDate().toString() + ", " + d.getFullYear().toString();
 }
