0f4bd79d3d4cfbed867fa01c0a0aa9cd0e9c18ac
facasaca <franciscoccasaca@gmail.com>
2020-01-20 23:11:53 +0000

    feat: download and upload Amazon S3 bucket objects

--- server/services/amazon-services.js
+++ server/services/amazon-services.js
@@ -1,14 +1,81 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
 let dotenv = require('dotenv');
 const path = require('path')
-const AWS = require('aws-sdk');
+
 dotenv.config({path: path.join(__dirname,'../../.env')});
 dotenv.load();
-amazon_key = process.env.AMAZON_ACCESS_KEY;
-amazon_secret_key = process.env.AMAZON_SECRET_ACCESS_KEY;
+const amazon_key = process.env.AMAZON_ACCESS_KEY;
+const amazon_secret_key = process.env.AMAZON_SECRET_ACCESS_KEY;
+const bucket_name = process.env.AMAZON_S3_BUCKET_NAME;
 
 AWS.config.update({
     accessKeyId: amazon_key,
     secretAccessKey: amazon_secret_key
 });
 
-let S3 = module.exports = exports = new AWS.S3();
+var s3 = new AWS.S3();
+
+
+class AmazonServices {
+    constructor() {
+        this.s3 = s3;
+        this.bucketName = bucket_name;
+        this.downloadFile = downloadFile;
+        this.uploadFile = uploadFile;
+    }
+}
+
+
+let amazonServices = module.exports = exports = new AmazonServices();
+
+
+async function downloadFile(filePath, keyName) {
+    
+    // Preparing file to be written
+    let file = fs.createWriteStream(filePath);
+
+    // Setting up S3 download parameters
+    let params = {
+        Bucket: this.bucketName,
+        Key: keyName
+    }
+
+    // Downloading file from bucket
+    return new Promise((resolve, reject) => {
+        this.s3.getObject(params).createReadStream()
+        .on('end', () => {
+            console.log('Downloaded file', keyName, 'from S3 bucket', this.bucketName);
+            return resolve();
+        })
+        .on('error', (error) => {
+            return reject(error);
+        }).pipe(file);
+    });
+}
+
+async function uploadFile(filePath, keyName) {
+
+    // Read content from file
+    const fileContent = fs.readFileSync(filePath);
+
+    // Setting up S3 upload parameters
+    let params = {
+        Bucket: this.bucketName,
+        Key: keyName,
+        Body: fileContent
+    }
+
+    // Uploading file to bucket
+    return new Promise((resolve, reject) => {
+        this.s3.upload(params, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            if (data) {
+                console.log('Uploaded file', keyName, 'to S3 bucket', this.bucketName);
+                return resolve();
+            }
+        });
+    });
+}
